fb0876e0538df78320acdc8a36d51af4d820c064
Maximiliano Rodriguez <maximilianos.rdgz@gmail.com>
2016-09-25 23:04:46 -0300

    SQL Connection added

--- src/com/weatherCheck/main/Main.java
+++ src/com/weatherCheck/main/Main.java
@@ -1,9 +1,38 @@
 package com.weatherCheck.main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Created by MaxPower on 23/09/2016.
  */
 public class Main {
     public static void main(String [] args){
+
+
+
+            String url = "jdbc:sqlserver://localhost:1433;" +
+                    "databaseName=WeatherCheckTest;user=MaxPower;password=";
+
+            Connection con=null;
+            Statement stmnt=null;
+            ResultSet rs=null;
+
+        try {
+            // Establish the connection.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(url);
+
+            // Create and execute an SQL statement that returns some data.
+            String SQL = "SELECT TOP 10 * FROM Person.Contact";
+            stmnt = con.createStatement();
+            rs = stmnt.executeQuery(SQL);
+        }
+        catch (Exception e){
+
+        }
+
+
     }
 }
